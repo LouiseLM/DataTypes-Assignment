9ef1ef73bf16b1bf77783b756d0051c952d1631f
LouiseLM <llpmoeller@gmail.com>
2022-11-26 17:10:44 +0100

    Fixed to better adhere to given requirements

--- src/Assignment_2/Main.java
+++ src/Assignment_2/Main.java
@@ -1,10 +1,10 @@
 package Assignment_2;
 
 public class Main {
-    private static final int number = 8;
-    private static final int number2 = 20;
 
     public static void main(String[] args) {
+        int number = 8;
+        int number2 = 20;
         int result = number * number2;
         System.out.println(result); // 160
     }
